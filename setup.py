cbc440fbbdb3d147f3fd53570c4cc08bbda81e56
Jorge Batista <dwjorgeb@hotmail.com>
2018-08-28 15:06:59 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,30 +3,30 @@
 
 from setuptools import setup, find_packages
 
-import materializecssform
+import selecmat
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
 
-    name='django-materializecss-form',
+    name='selecmat',
 
     version=materializecssform.__version__,
 
     packages=find_packages(),
 
-    author="Kal Walkden",
+    author="Jorge Batista",
 
-    author_email="kal@walkden.us",
+    author_email="jorge.batista@route.technology",
 
-    description="A simple Django form template tag to work with Materializecss",
+    description="A simple Django form template tag to work with Materializecss and Selectize.js",
     long_description=long_description,
     long_description_content_type="text/markdown",
 
     include_package_data=True,
 
-    url='https://github.com/kalwalkden/django-materializecss-form',
+    url='https://github.com/dwjorgeb/selecmat',
     classifiers=[
         "Programming Language :: Python",
         "Development Status :: 4 - Beta",
